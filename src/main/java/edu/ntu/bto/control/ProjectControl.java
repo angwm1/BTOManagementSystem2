ae54fae56c140de3c30609fc3160f6497bd8827d
afn2905 <53172772+afn2905@users.noreply.github.com>
2025-04-19 17:34:38 +0800

    Update ProjectControl.java

--- src/main/java/edu/ntu/bto/control/ProjectControl.java
+++ src/main/java/edu/ntu/bto/control/ProjectControl.java
@@ -1,1 +1,72 @@
+package edu.ntu.bto.control;
+
+import java.util.ArrayList;
+
+import edu.ntu.bto.model.Applicant;
+import edu.ntu.bto.model.Project;
+import java.util.List;
+
+public class ProjectControl {
+    private List<Project> projects;
+
+    public ProjectControl(List<Project> projects) {
+        this.projects = projects;
+    }
+
+    public List<Project> getVisibleProjectsForApplicant(Applicant applicant) {
+        List<Project> result = new ArrayList<>();
+        for (Project p : projects) {
+            if (!p.isVisible()) continue;
+            if (applicant.getMaritalStatus().equalsIgnoreCase("Single") && applicant.getAge() >= 35) {
+                if (p.getUnitsType1() != 0) result.add(p);
+            } else if (applicant.getMaritalStatus().equalsIgnoreCase("Married") && applicant.getAge() >= 21) {
+                result.add(p);
+            }
+        }
+        return result;
+    }
+
+    public List<Project> filterByNeighborhood(String keyword) {
+        List<Project> result = new ArrayList<>();
+        for (Project p : projects) {
+            if (p.getNeighborhood().equalsIgnoreCase(keyword)) result.add(p);
+        }
+        return result;
+    }
+
+    public List<Project> filterByFlatType(String flatType) {
+        List<Project> result = new ArrayList<>();
+        for (Project p : projects) {
+            if (flatType.equalsIgnoreCase(p.getType1())) {
+                if (p.getUnitsType1() != 0) {
+                    result.add(p);
+                }
+            }
+            else {
+                if (p.getUnitsType2() != 0) {
+                    result.add(p);
+                }
+            }
+        }
+        return result;
+    }
+
+    public void toggleProjectVisibility(Project project) {
+        project.toggleVisibility();
+    }
+
+    public List<Project> getAllProjects() {
+        return projects;
+    }
+
+    public List<Project> getProjectsByManager(String manager) {
+        List<Project> result = new ArrayList<>();
+        for (Project p : projects) {
+            if (p.getManager().equals(manager)) {
+                result.add(p);
+            }
+        }
+        return result;
+    }
+}
 
