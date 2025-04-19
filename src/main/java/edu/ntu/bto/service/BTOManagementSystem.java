e786098ce230c94fd2986c88988ed26adf311e5e
afn2905 <53172772+afn2905@users.noreply.github.com>
2025-04-19 17:41:12 +0800

    Update BTOManagementSystem.java

--- src/main/java/edu/ntu/bto/service/BTOManagementSystem.java
+++ src/main/java/edu/ntu/bto/service/BTOManagementSystem.java
@@ -2,6 +2,12 @@ package edu.ntu.bto.service;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import edu.ntu.bto.control.ProjectControl;
+import edu.ntu.bto.control.ApplicationControl;
+import edu.ntu.bto.control.OfficerControl;
+import edu.ntu.bto.control.ManagerControl;
+import edu.ntu.bto.control.EnquiryControl;
 import edu.ntu.bto.model.*;
 import edu.ntu.bto.util.DataLoader;
 
@@ -11,10 +17,21 @@ import edu.ntu.bto.util.DataLoader;
 public class BTOManagementSystem {
     private List<User> users;
     private List<Project> projects;
+    private List<Application> applications;
+    private List<Registration> registrations;
+    private List<Enquiry> enquiries;
+    private final ProjectControl pc;
+    private final ApplicationControl ac;
+    private final OfficerControl oc;
+    private final ManagerControl mc;
+    private final EnquiryControl ec;
     
     public BTOManagementSystem() {
         users = new ArrayList<>();
         projects = new ArrayList<>();
+        applications = new ArrayList<>();
+        registrations = new ArrayList<>();
+        enquiries = new ArrayList<>();
         
         // File names – DataLoader expects these in the "data" folder.
         String applicantFile = "ApplicantList.xlsx";
@@ -29,12 +46,43 @@ public class BTOManagementSystem {
         
         // Load projects.
         projects.addAll(DataLoader.loadProjects(projectFile));
+
+        // Setup control
+        pc = new ProjectControl(projects);
+        ac = new ApplicationControl(applications);
+        oc = new OfficerControl(registrations);
+        mc = new ManagerControl(projects, applications, registrations);
+        ec = new EnquiryControl(enquiries);
         
         System.out.println("Data loaded: " + users.size() + " users, " + projects.size() + " projects.");
     }
     
     public List<Project> getProjects() {
         return projects;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public ProjectControl getProjectControl() {
+        return pc;
+    }
+
+    public ApplicationControl getApplicationControl() {
+        return ac;
+    }
+
+    public OfficerControl getOfficerControl() {
+        return oc;
+    }
+
+    public ManagerControl getManagerControl() {
+        return mc;
+    }
+
+    public EnquiryControl getEnquiryControl() {
+        return ec;
     }
     
     public void addProject(Project proj) {
