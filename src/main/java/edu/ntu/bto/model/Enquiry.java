419562737b83b67fcde736b6df888f3fa2361f1f
afn2905 <53172772+afn2905@users.noreply.github.com>
2025-04-19 17:36:30 +0800

    Update Enquiry.java

--- src/main/java/edu/ntu/bto/model/Enquiry.java
+++ src/main/java/edu/ntu/bto/model/Enquiry.java
@@ -4,28 +4,46 @@ package edu.ntu.bto.model;
  * Represents an enquiry submitted by a user.
  */
 public class Enquiry {
-    private String text;
-    private String userNric;
+    private String id;
+    private Applicant applicant;
+    private Project project;
+    private String question;
+    private String response;
     
-    public Enquiry(String text, String userNric) {
-        this.text = text;
-        this.userNric = userNric;
+    public Enquiry(String id, Applicant applicant, Project project, String question) {
+        this.id = id;
+        this.applicant = applicant;
+        this.project = project;
+        this.question = question;
+        this.response = null;
     }
     
-    public String getText() {
-        return text;
+    public String getId() { 
+        return id; 
     }
-    
-    public void setText(String text) {
-        this.text = text;
+
+    public String getQuestion() { 
+        return question; 
     }
-    
-    public String getUserNric() {
-        return userNric;
+
+    public Applicant getApplicant() { 
+        return applicant; 
+    }
+
+    public Project getProject() { 
+        return project; 
+    }
+
+    public String getResponse() { 
+        return response; 
+    }
+
+    public void setResponse(String response) { 
+        this.response = response; 
     }
     
     @Override
     public String toString() {
-        return "Enquiry from " + userNric + ": " + text;
+        return "Enquiry from " + applicant.getNric() + ": " + question;
     }
 }
