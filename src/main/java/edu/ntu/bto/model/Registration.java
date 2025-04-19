ecd167a242808846075d73bd7c306028261c8176
afn2905 <53172772+afn2905@users.noreply.github.com>
2025-04-19 17:39:47 +0800

    Update Registration.java

--- src/main/java/edu/ntu/bto/model/Registration.java
+++ src/main/java/edu/ntu/bto/model/Registration.java
@@ -1,1 +1,34 @@
+package edu.ntu.bto.model;
+
+public class Registration {
+    public enum Status {
+        PENDING, APPROVED, REJECTED
+    }
+
+    private HDBOfficer officer;
+    private Project project;
+    private Status status;
+
+    public Registration(HDBOfficer officer, Project project) {
+        this.officer = officer;
+        this.project = project;
+        this.status = Status.PENDING;
+    }
+
+    public Status getStatus() { 
+        return status; 
+    }
+
+    public void setStatus(Status status) { 
+        this.status = status; 
+    }
+
+    public HDBOfficer getOfficer() { 
+        return officer; 
+    }
+
+    public Project getProject() { 
+        return project; 
+    }
+}
 
