44d66cef7ad6c5be4d27e7732407ba294a456aaa
afn2905 <53172772+afn2905@users.noreply.github.com>
2025-04-19 17:36:09 +0800

    Update Application.java

--- src/main/java/edu/ntu/bto/model/Application.java
+++ src/main/java/edu/ntu/bto/model/Application.java
@@ -1,1 +1,40 @@
+package edu.ntu.bto.model;
+
+public class Application {
+    public enum Status {
+        PENDING, SUCCESSFUL, UNSUCCESSFUL, BOOKED
+    }
+
+    private Applicant applicant;
+    private Project project;
+    private Status status;
+    private String flatType;
+
+    public Application(Applicant applicant, Project project, String flatType) {
+        this.applicant = applicant;
+        this.project = project;
+        this.flatType = flatType;
+        this.status = Status.PENDING;
+    }
+
+    public Status getStatus() { 
+        return status; 
+    }
+
+    public void setStatus(Status status) { 
+        this.status = status; 
+    }
+
+    public String getFlatType() { 
+        return flatType; 
+    }
+
+    public Applicant getApplicant() { 
+        return applicant; 
+    }
+
+    public Project getProject() { 
+        return project; 
+    }
+}
 
